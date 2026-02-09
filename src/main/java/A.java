528b47dd257537c6af7e51c27b15001556111c58
Divyangichaudhari <DivyangiChaudhari0705>
2026-02-09 12:52:02 +0530

    A class changees

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -5,5 +5,8 @@ public class A {
 		System.out.println("Hi A1");
 		System.out.println("Hi A2 ");
 		System.out.println("Hi A3 ");
+		System.out.println("Hello A7");
+		System.out.println("Hello A3 ");
+		System.out.println("Hello A9 ");
 	}
 }
