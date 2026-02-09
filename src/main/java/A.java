2989b304999dfcf4546b62692f32c3aabf7a6ae5
smitavqa <smitav.qa@gmail.com>
2026-02-09 12:22:50 +0530

    Add Maven project source code

---
+++ src/main/java/A.java
@@ -0,0 +1,9 @@
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("Hi from A");
+		System.out.println("Hi A1");
+		System.out.println("Hi A2 ");
+		System.out.println("Hi A3 ");
+	}
+}
